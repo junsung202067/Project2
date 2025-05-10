a93d94c00c10758f32c6a705ae93f79ea449a610
JJJangJeongSu <jason808@naver.com>
2025-05-10 14:42:34 +0900

    파일 크기 비교하는 함수 구현 완료

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 ﻿#include <stdio.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 struct stat stat1, stat2;
 struct tm *time1, *time2;
@@ -16,53 +16,45 @@ void blockcmp();
 void datecmp();
 void timecmp();
 
-int main(){
-    filestat1();
-    filestat2();
-    filetime1();
-    filetime2();
-    sizecmp();
-    blockcmp();
-    datecmp();
-    timecmp();
+int main() {
+  filestat1();
+  filestat2();
+  filetime1();
+  filetime2();
+  sizecmp();
+  blockcmp();
+  datecmp();
+  timecmp();
 }
 
-//파일 1의 정보를 가져오는 함수 작성
-void filestat1(){
-    
+// 파일 1의 정보를 가져오는 함수 작성
+void filestat1() {}
+
+// 파일 2의 정보를 가져오는 함수 작성
+void filestat2() {}
+
+// 파일 1의 시간 정보를 가져오는 함수 작성
+void filetime1() {}
+
+// 파일 2의 시간 정보를 가져오는 함수 작성
+void filetime2() {}
+
+// 두 개의 파일 크기를 비교하는 함수 작성
+void sizecmp() {
+  if (stat1.st_size > stat2.st_size) {
+    printf("File 1 is larger than File 2\n");
+  } else if (stat1.st_size < stat2.st_size) {
+    printf("File 2 is larger than File 1\n");
+  } else {
+    printf("Both files are of the same size\n");
+  }
 }
 
-//파일 2의 정보를 가져오는 함수 작성
-void filestat2(){
-    
-}
+// 두 개의 파일 블락 수를 비교하는 함수 작성
+void blockcmp() {}
 
-//파일 1의 시간 정보를 가져오는 함수 작성
-void filetime1(){
-    
-}
+// 두 개의 파일 수정 날짜를 비교하는 함수 작성
+void datecmp() {}
 
-//파일 2의 시간 정보를 가져오는 함수 작성
-void filetime2(){
-    
-}
-
-//두 개의 파일 크기를 비교하는 함수 작성
-void sizecmp(){
-    
-}
-
-//두 개의 파일 블락 수를 비교하는 함수 작성
-void blockcmp(){
-    
-}
-
-//두 개의 파일 수정 날짜를 비교하는 함수 작성
-void datecmp(){
-    
-}
-
-//두 개의 파일 수정 시간을 비교하는 함수 작성
-void timecmp(){
-    
-}
+// 두 개의 파일 수정 시간을 비교하는 함수 작성
+void timecmp() {}
